2ec1083a0e6c768f2c0555bdbbd999de66ae5f50
Germain Rwibutso <the13nth@gmail.com>
2018-10-30 20:28:12 +0200

    last push

--- Server.js
+++ Server.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var app = express();
-var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'dist/Minimus')));
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('dist/Minimus', function(req,res){
-   res.sendFile('index.html', {root: path.join(__dirname, './dist/Minimus')})
-});
-
-app.listen(8080, function(){
-    console.log((new Date()) + " Server is listening on port 8080");
-});
+express()
+  .use(express.static(path.join(__dirname, 'dist/Minimus')))
+  .get('/', (req, res) => res.render('dist/Minimus/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
