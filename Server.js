103f6153be0d2e252fd5aa0389c9a835b5a47c3d
Germain Rwibutso <the13nth@gmail.com>
2018-10-30 20:01:29 +0200

    last push

--- Server.js
+++ Server.js
@@ -1,23 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'dist/Minimus')));
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist/Minimus'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
+app.get('/', function(req,res){
+   res.sendFile('index.html', {root: path.join(__dirname, './dist/Minimus')})
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(8080, function(){
+    console.log((new Date()) + " Server is listening on port 8080");
 });
